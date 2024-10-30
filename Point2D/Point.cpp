bd1319e19419724960072300a6711b5b6f12654a
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-10-29 21:02:06 -0700

    Initial commit for Point2D project
    - Added Point.hpp: Defines the Point class with x and y coordinates
    - Added Point.cpp: Implements Point methods and overloads equality (==) and stream insertion (<<) operators
    - Added main.cpp: Demonstrates functionality with test cases for the Point class
    - Added README.md: Project overview, usage instructions, and example output
    
    This commit sets up the core structure of the Point2D project with a focus on 2D point representation, operator overloading, and basic functionality tests.

---
+++ Point2D/Point.cpp
@@ -0,0 +1,8 @@
+//
+//  Point.cpp
+//  Point2D
+//
+//  Created by Isaac Adeleke on 10/29/24.
+//
+
+#include "Point.hpp"
---
+++ Point2D/Point.hpp
@@ -0,0 +1,13 @@
+//
+//  Point.hpp
+//  Point2D
+//
+//  Created by Isaac Adeleke on 10/29/24.
+//
+
+#ifndef Point_hpp
+#define Point_hpp
+
+#include <stdio.h>
+
+#endif /* Point_hpp */
