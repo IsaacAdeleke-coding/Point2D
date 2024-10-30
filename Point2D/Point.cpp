33e51afc51941efd99f452eff870e4b4903d0747
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-10-29 21:03:18 -0700

    Initial commit for Point2D project
    - Added Point.hpp: Defines the Point class with x and y coordinates
    - Added Point.cpp: Implements Point methods and overloads equality (==) and stream insertion (<<) operators
    - Added main.cpp: Demonstrates functionality with test cases for the Point class
    - Added README.md: Project overview, usage instructions, and example output
    
    This commit sets up the core structure of the Point2D project with a focus on 2D point representation, operator overloading, and basic functionality tests.

--- Point2D/Point.cpp
+++ Point2D/Point.cpp
@@ -6,3 +6,27 @@
 //
 
 #include "Point.hpp"
+
+// Constructor
+Point::Point(int xCoord, int yCoord) : x(xCoord), y(yCoord) {}
+
+// Getter methods
+int Point::getX() const {
+    return x;
+}
+
+int Point::getY() const {
+    return y;
+}
+
+// Overloaded equality operator (==)
+bool Point::operator==(const Point& other) const {
+    return (x == other.x) && (y == other.y);
+}
+
+// Overloaded stream insertion operator (<<)
+std::ostream& operator<<(std::ostream& os, const Point& point) {
+    os << "(" << point.x << ", " << point.y << ")";
+    return os;
+}
+
--- Point2D/Point.hpp
+++ Point2D/Point.hpp
@@ -5,9 +5,29 @@
 //  Created by Isaac Adeleke on 10/29/24.
 //
 
-#ifndef Point_hpp
-#define Point_hpp
+#ifndef POINT_HPP
+#define POINT_HPP
 
-#include <stdio.h>
+#include <iostream>
 
-#endif /* Point_hpp */
+class Point {
+private:
+    int x;
+    int y;
+
+public:
+    // Constructor with default parameters
+    Point(int xCoord = 0, int yCoord = 0);
+
+    // Getters for x and y coordinates
+    int getX() const;
+    int getY() const;
+
+    // Overload the equality operator (==)
+    bool operator==(const Point& other) const;
+
+    // Overload the stream insertion operator (<<)
+    friend std::ostream& operator<<(std::ostream& os, const Point& point);
+};
+
+#endif // POINT_HPP
